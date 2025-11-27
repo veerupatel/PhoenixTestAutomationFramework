2723bf2a4382c66265885f01fbe0ba6379e3960c
veerupatel <birajup01@gmail.com>
2025-11-27 10:06:08 +0530

    Introduced LoginAPI Data Driven test for poc of open CSV Library!

---
+++ src/test/java/com/api/tests/datadriven/LoginAPITest.java
@@ -0,0 +1,36 @@
+package com.api.tests.datadriven;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import com.api.utils.ConfigManager;
+import com.api.utils.ConfigManager2;
+import com.api.utils.SpecUtil;
+import com.dataproviders.api.bean.UserBean;
+import com.request.models.UserCredentials;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class LoginAPITest {
+	
+	
+	@Test(description = "Verifying if login api is working for FD user"
+			,groups= {"api","regression","smoke","datadriven"}
+	,dataProviderClass = com.dataproviders.DataProviderUtils.class,dataProvider = "LoginAPIDataProvider")
+	public void loginAPITest(UserBean userbean) {
+		
+		RestAssured.given()
+		.spec(SpecUtil.requestSpec(userbean))
+		.when()
+		.post("login")
+		.then()
+		.spec(SpecUtil.resposeSpec_OK())
+		.body("message", Matchers.equalTo("Success"))
+		.body("data.token", Matchers.notNullValue())
+		.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-Schema/LoginAPIResponseSchema.json"))
+		.extract().response();
+	}
+
+}
--- src/test/java/com/api/utils/CSVreaderUtility.java
+++ src/test/java/com/api/utils/CSVreaderUtility.java
@@ -1,19 +1,23 @@
-package com.demo.csv;
+package com.api.utils;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.List;
 
+import com.dataproviders.api.bean.UserBean;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvException;
 
-public class CSVPractices {
-
-	public static void main(String[] args) throws IOException, CsvException {
-		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData\\loginData.csv");
+public class CSVreaderUtility {
+	
+	private CSVreaderUtility() {
+		
+	}
+	
+	public static Iterator<UserBean> loadCSV(String pathOfCSVFile){
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
 		InputStreamReader inputStreamReader = new InputStreamReader(is);
 		CSVReader csvReader = new CSVReader(inputStreamReader);
 		
@@ -24,13 +28,9 @@ public class CSVPractices {
 				.build();
 		
 		List<UserBean> userlist = csvToBean.parse();
-		System.out.println(userlist.get(0));
-		System.out.println(userlist.get(1));
-		
-		
-				
-		
+		return userlist.iterator();
+}
 
-	}
-
+	
+	
 }
