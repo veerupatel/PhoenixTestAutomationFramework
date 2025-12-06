7119206b6453cb39d9f7b4fd13dd7a4117e45f9a
Biraju <pbiraju71@gmail.com>
2025-12-06 22:41:24 +0530

    Created Date time utility

---
+++ src/test/java/com/api/utils/DateTimeUtil.java
@@ -0,0 +1,15 @@
+package com.api.utils;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class DateTimeUtil {
+
+	private DateTimeUtil() {
+
+	}
+
+	public static String getTimeWithDaysAgo(int days) {
+		return Instant.now().minus(days, ChronoUnit.DAYS).toString();
+	}
+}
